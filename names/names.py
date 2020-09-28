c5356060dc4db2c49466ed1ed3c2d4aa56808d51
taty2010 <shydalys123@gmail.com>
2020-09-27 20:02:51 -0500

    mvp

--- names/names.py
+++ names/names.py
@@ -1,5 +1,40 @@
 import time
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    def insert(self, value):
+        if value < self.value:
+            # if self.left is None:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            # if self.right is None:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+    def contains(self, target):
+        if not self.value:
+            return False
+        if target == self.value:
+            return True
+        else:
+            if target < self.value:
+                if not self.left:
+                    return False
+                else:
+                    return self.left.contains(target)
+            else:
+                if not self.right:
+                    return False
+                else:
+                    return self.right.contains(target)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,26 +48,20 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-class BSTNode:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-        self.duplicate = []
-    def insert(self, value):
-        if self.left is None:
-            self.left = BSTNode(value)
-        else:
-            self.left.insert(value)
-    def contains(self):
-        if self.left.value is names_2:
-            duplicates.append(self.left.value)
+bst = BSTNode(names_1[0])
+for name in names_1:
+    bst.insert(name)
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
+
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-BSTNode(1).insert(names_1)
+# BSTNode(1).insert(names_1)
+# BSTNode(1).contains(names_2)
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,15 +3,28 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.storage = []
+        self.current = 0
     def append(self, item):
         c = len(self.storage)
-        if c < self.capacity:
-            self.storage.append(item)
         if c is self.capacity:
-            current = self.storage[0]
-            next_item = 0
-            self.storage.pop(0)
+            self.storage[self.current]= item
+            if self.current + 1 is self.capacity:
+                self.current = 0
+            else:
+                self.current +=1
+        else: 
             self.storage.append(item)
+            return item
+        # if c < self.capacity:
+        #     self.storage.append(item)
+        # if c is self.capacity:
+        #     current = self.storage[0]
+        #     next_item = 0
+        #     while next_item is not self.capacity:
+        #         next_item = next_item + 1 
+        #         print(next_item)
+        #     self.storage.pop(next_item - 1)
+        #     self.storage.insert(next_item, item)
         # if c is not self.capacity:
         #     self.storage.append(item)
         # elif c is self.capacity:
